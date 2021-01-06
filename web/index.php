6216102266879b8bc8ec0185e06d4a5fd9e226b4
kaedenbrinkman <49798040+kaedenbrinkman@users.noreply.github.com>
2021-01-05 17:57:00 -0800

    Initial commit

--- web/index.php
+++ web/index.php
@@ -1,19 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include 'src/assets/head.php';
+?>
+<div class="container">
+    <h1>Hello, world!</h1>
+</div>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tesla</title>
-    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="/src/css/main.css">
-    <script type="text/javascript" src="/src/js/script.js"></script>
-</head>
 
-<body>
-
-</body>
-
-</html>
+<?php
+include 'src/assets/foot.php';
+?>
---
+++ web/src/assets/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tesla</title>
+    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="/src/css/main.css">
+    <script type="text/javascript" src="/src/js/script.js"></script>
+</head>
+
+<body>
