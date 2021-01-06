39d13bbf0805d5752d5830ac7e7a671f7b14835c
kaedenbrinkman <49798040+kaedenbrinkman@users.noreply.github.com>
2021-01-05 18:26:20 -0800

    Added stephangroen/tesla-php-client

---
+++ web/src/assets/client.php
@@ -0,0 +1,6 @@
+<?php
+$tesla = new StephanGroen\Tesla\Tesla();
+$tesla->setClientId('client_id_here');
+$tesla->setClientSecret('client_secret_here');
+session_start();
+
--- web/src/assets/head.php
+++ web/src/assets/head.php
@@ -1,3 +1,6 @@
+<?php
+include 'client.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
